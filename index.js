bb282cd3760832223b246051951fdca3060e7178
janineebaltazar <janine.ebaltazar@gmail.com>
2018-08-08 18:28:03 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -3,7 +3,9 @@ function scuberGreetingForFeet(value){
   if(value <= 400){
     return "This one is on me!";
   }
-  else if(value > 2000)
+  else if(value > 2000){
+    
+  }
 }
 
 scuberGreetingForFeet(199);
